0d337124aeb601beb8c9e0ad63eb3de9a53e9be9
staplezz <kokofrank@ciencias.unam.mx>
2018-02-14 21:25:37 -0600

    más funciones...

--- práctica01.hs
+++ práctica01.hs
@@ -8,3 +8,19 @@ deriva a b c x = (2*a*x) + b
 --Calcula el área de un cilindro.
 areaCilindro:: Float -> Float -> Float
 areaCilindro r h = 2*pi*r*(r + h)
+
+--Calcula el volúmen de un cilindro.
+volumenCilindro:: Float -> Float -> Float
+volumenCilindro r h = 2*pi*r^2*h
+
+--Realiza operaciones varias dependiendo de el caractér que le pasan.
+aplicaOperacion:: Char -> Int -> Int -> Int
+aplicaOperacion a b c =
+	case a of
+		's' -> b
+		't' -> c
+		'a' -> b + c
+		'r' -> b - c
+		'p' -> b*c
+		'd' -> b `div` c
+		'e' -> b^c
