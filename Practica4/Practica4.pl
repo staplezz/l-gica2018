04453df222b7c62c55b1db268fdf9b15817dcf7d
Ulilop <ulises.lopez@ciencias.unam.mx>
2018-04-26 20:50:10 -0500

    Creado esqueleto.

---
+++ Practica4/Practica4.pl
@@ -0,0 +1,47 @@
+/*******************************************************************************
+*  Practica4.pl - v1.0                                                         *
+*                                                                              *
+*                             Lógica Computacional                             *
+*                     Facultad de Ciencias, UNAM, 2018-2                       *
+*                        Todos los derechos reservados                         *
+*                                                                              *
+*                             Práctica 4: Prolog                               *
+*                                                                              *
+*  Objetivos y Anotaciones                                                     *
+*     Que el alumno utilice3 el lenguaje de programació PROLOG.                *
+*                                                                              *
+*  Profesor: Pilar Selene Linares Arévalo                                      *
+*  Ayudante: Daniela Calderón Pérez                                            *
+*  Ayud.Lab: Alejando Hernández Mora                                           *
+*  Ayud.Lab: Luis Manuel Martínez Damaso                                       *
+*                                                                              *
+*******************************************************************************/
+
+/***
+  @descr <p> Programa para realizar lo ejercicios de la prática 4. </p>
+    <ul>
+      <li> Ver P04.pdf </li>
+    </ul>
+  @author Cortés López Jorge Francisco
+  @author López Arias Victor Ulises
+  @author Sainz Takata Izumi María
+  @date 26/04/2018
+*/
+
+/**
+  @form e(x)
+    @constraints
+      <ul>
+        <li>x es </li>
+      </ul>
+   @descr ...
+*/
+
+/*******************************************************************************
+*  Fin Practica4.pl                                                            *
+*                                                                              *
+*  Cortés López Jorge Francisco  314330981   kokofrank@ciencias.unam.mx        *
+*  López Arias Víctor Ulises     310173335   ulises.lopez@ciencias.unam.mx     *
+*  Sainz Takata Izumi María      314245195   sainz@ciencias.unam.mx            *
+*                                                                              *
+*******************************************************************************/
